2d09e3cd61877d8ec21e37761e6145e1ad2329ec
JulioCisner0s <juliocesarcisnerosrosales@gmail.com>
2024-07-03 18:03:59 -0600

    segunda_preentrega_cisneros

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="segunda_preentrega_cisneros",
+    version="1.0",
+    description="Programa de gestion de clientes",
+    author="Julio Cisneros Rosales",
+    author_email="juliocesarcisnerosrosales@gmail.com",
+    
+    packages=["segunda_preentrega_cisneros"],
+)
